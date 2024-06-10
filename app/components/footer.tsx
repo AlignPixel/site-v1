7162725238d7b324c5abeecc66876c57c9f08a43
Fengyi Chen <seventeen@ohdragonboi.cn>
2024-06-10 14:38:51 +0800

    Fix

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -15,16 +15,6 @@ function ArrowIcon() {
   )
 }
 
-function RssIcon() {
-  return (
-    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-rss">
-      <path d="M4 11a9 9 0 0 1 9 9"></path>
-      <path d="M4 4a16 16 0 0 1 16 16"></path>
-      <circle cx="5" cy="19" r="1"></circle>
-    </svg>
-  )
-}
-
 export default function Footer() {
   return (
     <footer className="mb-16">
@@ -36,7 +26,8 @@ export default function Footer() {
             target="_blank"
             href="/rss"
           >
-            <RssIcon />
+            <ArrowIcon />
+            <p className="ml-2 h-7">Rss</p>
           </a>
         </li>
         <li>
